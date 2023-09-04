83f7d46eb218edcbcc6f34444902cd22e7b7f231
Jason Andrews <Blaqusani@gmail.comgit --edit --globalgit config --global user.email Blaqusani@gmail.com>
2023-09-04 05:54:55 -0400

    fixed structures on pie and cake pages

--- js/script.js
+++ js/script.js
@@ -1,54 +1,38 @@
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector(".carousel");
-    const slides = document.querySelectorAll(".slide");
-    let currentIndex = 0;
-
-
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            console.log(currentIndex)
-            slide.style.transform = `translateX(${100 * (i - index)}%)`;
-            console.log(currentIndex)
-        });
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        console.log(currentIndex)
-        showSlide(currentIndex);
-        console.log(currentIndex)
-    }
-
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
-    }
-
-    // Start the carousel after all images are loaded
-    const images = document.querySelectorAll(".slide img");
-    let loadedImages = 0;
-    
-
-    images.forEach((image) => {
-        image.addEventListener("load", function() {
-            loadedImages++;
-            if (loadedImages === images.length) {
-                showSlide(currentIndex);
-                setInterval(nextSlide, 3000);
-                console.log(currentIndex)
-            }
-        });
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
     });
+}
 
-    // Add event listeners for next and previous buttons (if you have them)
-    document.querySelector(".next-button").addEventListener("click", nextSlide);
-    document.querySelector(".prev-button").addEventListener("click", prevSlide);
-    
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+    resetCaroselTimer();
+}
 
-});
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+    resetCaroselTimer();
+}
 
- 
+// No needs to add an event listener to the page load. This will run when the js file is loaded.
+let slideTimer = setInterval(nextSlide, 3000); // Setting the interval to a variable allows you to store a reference to it so you can manipulate it later.
 
- 
+resetCaroselTimer = () => {
+    clearInterval(slideTimer); //Passing in the slide interval to the clearInterval func will delete the current interval.
+    slideTimer = setInterval(nextSlide, 3000); // Reset the interval by reassinging it to "slideTimer"
+}
+
+document.querySelector(".next-button").addEventListener("click", nextSlide);
+document.querySelector(".prev-button").addEventListener("click", prevSlide);
+
+
+
+
+
+
+
